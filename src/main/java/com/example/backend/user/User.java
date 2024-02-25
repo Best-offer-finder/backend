83d1be42d3b0148e55d5d0813a99cb4a2c87dcd7
Bartosz Malec <bartosz.malec@onet.pl>
2024-02-25 22:01:45 +0100

    Configure database

---
+++ src/main/java/com/example/backend/user/User.java
@@ -0,0 +1,53 @@
+package com.example.backend.user;
+
+import jakarta.persistence.*;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="user_id")
+    private Integer id;
+
+    @Column(nullable = false, length = 32)
+    private String name;
+
+    @Column(nullable = false, length = 60)
+    private String email;
+
+    @Column(length = 64)
+    private String password;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
